dddc6b17e618fb7018d7ed4632452c3946accb32
Lada Zednik <lzednik@gmail.com>
2015-08-12 22:08:31 -0500

    update

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ M.addArc({'arc9':{'spec':(6,7),'l':1,'t':1}})
 M.addArc({'arc10':{'spec':(7,8),'l':1,'t':1}})
 
 
-iti=['arc1','arc2','arc4','arc5','arc6','arc3','arc4','arc5','arc7','arc9','arc10']
+iti=['arc1','arc2','arc4','arc5','arc6','arc3','arc4','arc5','arc7','arc8','arc5','arc7','arc9','arc10']
 
 
 #print route
@@ -28,27 +28,29 @@ print route
 pos=0
 posmax=len(iti)-1
 
-newIti=[]
+toRemovePos=[]
 while pos<posmax:
     c= M.Arcs[iti[pos]]['spec'][0]
     pos2=pos+1
     foundLoop=False
     while pos2<posmax and foundLoop==False:
         if c== M.Arcs[iti[pos2]]['spec'][1]:
-            print 'loop found at pos ' +str(pos)
+            print 'loop found at pos ' +str(pos)+ ' ' +str(pos2)
+            for x in range (pos,pos2+1):
+                toRemovePos.append(x)
             foundLoop=True
-            break
-        pos2+=1
-    if foundLoop!=True:
-        newIti.append(iti[pos])
-        pos+=1
-    else:
-        newIti.append(iti[pos2+1])
-        pos=pos2+1
+            pos=pos2+1
+        else:
+            pos2+=1
+    pos+=1
 
+#delete loops
+toRemovePos.sort(reverse=True)
+for x in toRemovePos:
+    del iti[x]
 
 #print route
 route=''
-for arc in newIti:
+for arc in iti:
     route+= str(M.Arcs[arc]['spec'])+ '  '
 print route
